e3301cce386944e4cda4aa6238e8d33e84b13ad6
Matt <matt@pop-os.localdomain>
2020-08-30 13:44:45 -0400

    Updates

---
+++ precache-manifest.41a39a909d49215b2212c6b223e4a9e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf0676192dc7a782e24fdd636048479",
+    "url": "/portfolio_test/index.html"
+  },
+  {
+    "revision": "609da941f6af840d41f7",
+    "url": "/portfolio_test/static/css/main.ea7741dc.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio_test/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_test/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609da941f6af840d41f7",
+    "url": "/portfolio_test/static/js/main.3da8c6cc.chunk.js"
+  },
+  {
+    "revision": "38d1418c49497c45d3b9",
+    "url": "/portfolio_test/static/js/runtime-main.a1edfe3a.js"
+  },
+  {
+    "revision": "ce6ff6e91bbbf071c51e7cee31bb085e",
+    "url": "/portfolio_test/static/media/profile.ce6ff6e9.jpg"
+  },
+  {
+    "revision": "015fb4eb0dd4fb2c22ca7a27d5ab6038",
+    "url": "/portfolio_test/static/media/section-background.015fb4eb.svg"
+  }
+]);
